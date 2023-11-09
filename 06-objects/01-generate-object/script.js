b4b5788b0a211fd0d3d3b11538f0c388e7aa21bb
AudriusGrebliunas <audrius.grebliunas@gmail.com>
2023-11-09 16:15:08 +0100

    Exercices objects

---
+++ 06-objects/01-generate-object/script.js
@@ -0,0 +1,27 @@
+/* becode/javascript
+ *
+ * /06-objects/01-generate-object/script.js - 6.1: générer un object
+ *
+ * coded by leny@BeCode
+ * started at 08/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+
+    
+    document.getElementById("run").addEventListener("click", () => {
+        const Audrius = {
+            lastname: "Grebliunas",
+            firstname: "Audrius",
+            age: "22",
+            city: "Braine-l'Alleud",
+            country: "Belgium"
+        }
+        console.log(Audrius)
+
+    })
+    
+})();
